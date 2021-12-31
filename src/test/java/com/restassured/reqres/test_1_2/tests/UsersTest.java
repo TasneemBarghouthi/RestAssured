e6e1984ef6d323625ec3d1beb7b64de7573336d1
tbarghou <tasneem.barghouthi.ext@nokia.com>
2021-12-31 22:29:26 +0200

    Initial commit

---
+++ src/test/java/com/restassured/reqres/test_1_2/tests/UsersTest.java
@@ -0,0 +1,29 @@
+package com.restassured.reqres.test_1_2.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class UsersTest {
+
+    public static final String USERS_BASE_URL = "https://reqres.in/api/users/";
+    public static final String RESPONSE_USER_FORMAT = "The user with ID #%s is <%s> <%s>";
+    private static final String ID = "7";
+    public static final String ID_PATH = "data.id";
+    public static final String FIRST_NAME_PATH = "data.first_name";
+    public static final String LAST_NAME_PATH = "data.last_name";
+
+    @Test
+    public void getUsersTest(){
+
+        Response response = RestAssured.get(USERS_BASE_URL + ID);
+
+        response.then().assertThat().statusCode(200);
+
+        JsonPath path = response.body().jsonPath();
+
+        System.out.printf((RESPONSE_USER_FORMAT) + "%n", path.get(ID_PATH), path.get(FIRST_NAME_PATH), path.get(LAST_NAME_PATH));
+
+    }
+}
